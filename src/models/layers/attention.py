4c890642fed253e6270f61a6facb025c6e96de0c
Keith Dao <keith.dao.248@gmail.com>
2026-09-02 19:33:11 +1000

    feat(model): add causal masking to attention function.

--- src/models/layers/attention.py
+++ src/models/layers/attention.py
@@ -9,6 +9,8 @@ def scaled_dot_product_attention(
     query: torch.Tensor,
     key: torch.Tensor,
     value: torch.Tensor,
+    *,
+    is_causal: bool = False,
 ) -> torch.Tensor:
     """Computes the scaled dot product attention.
 
@@ -20,11 +22,20 @@ def scaled_dot_product_attention(
         value:
             The value tensor.
 
+    Keyword Args:
+        is_causal:
+            If true, apply the causal mask of negative infinity to the upper right triangle of the query key
+            product. Otherwise, masking is skipped.
+
     Returns:
         The scaled dot product attention tensor.
     """
     scale_factor = 1 / math.sqrt(key.shape[-1])
-    query_key_product = query @ key.transpose(-2, -1)
-    value_weights = torch.softmax(query_key_product * scale_factor, dim=-1)
+    query_key_product = query @ key.transpose(-2, -1) * scale_factor
+    if is_causal:
+        mask = torch.full((query.shape[-2], key.shape[-2]), -torch.inf, device=query.device).triu(diagonal=1)
+        query_key_product += mask
+
+    value_weights = torch.softmax(query_key_product, dim=-1)
 
     return value_weights @ value
